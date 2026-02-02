311ae0413368a94621af0623320ff2be016db234
Alasdair Morrison <alasdair.morrison1002@gmail.com>
2026-02-02 10:22:06 +0000

    basic PWM code for one motor

--- Testing/Testing.ino
+++ Testing/Testing.ino
@@ -1,9 +1,25 @@
-void setup() {
-  // put your setup code here, to run once:
+#define enA_A 2 //PWM Out Motor A
+#define in1_A 3 //Enable 1 Motor A
+#define in2_A 4 //Enable 2 Motor A
 
+void setup() {
+  pinMode(enA_A, OUTPUT);
+  pinMode(in1_A, OUTPUT);
+  pinMode(in2_A, OUTPUT);
+  // Set initial rotation direction for forwards
+  digitalWrite(in1_A, LOW);
+  digitalWrite(in2_A, HIGH);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int pwmOutput = 50;
+  analogWrite(enA_A, pwmOutput); // Send PWM signal to L298N Enable pin
 
+  digitalWrite(in1_A, HIGH);
+  digitalWrite(in2_A, LOW);
+  delay(20);
+
+  digitalWrite(in1_A, LOW);
+  digitalWrite(in2_A, HIGH);
+  delay(20);
 }
