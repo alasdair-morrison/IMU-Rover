76e82d1dcc18ad0aa1df6bc852ce0ab3c6dd4da9
Alasdair Morrison <alasdair.morrison1002@gmail.com>
2026-02-02 13:22:39 +0000

    added second motor control for testing

--- Testing/Testing.ino
+++ Testing/Testing.ino
@@ -1,14 +1,20 @@
 #define enA_A 6 //PWM Out Motor A
 #define in1_A 3 //Enable 1 Motor A
 #define in2_A 4 //Enable 2 Motor A
+#define in1_B 1 // Enable 1 Motor B
+#define in2_B 2 // Enable 2 Motor B
 
 void setup() {
   pinMode(enA_A, OUTPUT);
   pinMode(in1_A, OUTPUT);
   pinMode(in2_A, OUTPUT);
+  pinMode(in1_B, OUTPUT);
+  pinMode(in2_B, OUTPUT);
   // Set initial rotation direction for forwards
   digitalWrite(in1_A, LOW);
   digitalWrite(in2_A, HIGH);
+  digitalWrite(in1_B, LOW);
+  digitalWrite(in2_B, HIGH);
 }
 
 void loop() {
@@ -17,6 +23,8 @@ void loop() {
 
   digitalWrite(in1_A, HIGH);
   digitalWrite(in2_A, LOW);
+  digitalWrite(in1_B, HIGH);
+  digitalWrite(in2_B, LOW);
   delay(20);
   
   //digitalWrite(in1_A, LOW);
